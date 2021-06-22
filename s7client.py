7b85b69fdc80db41949c6f1d6df2e83ba58aeaf5
Sarat Raj <saratraj.batchu@stfc.ac.uk>
2021-06-22 12:57:21 +0530

    q0.0

--- s7client.py
+++ s7client.py
@@ -3,15 +3,8 @@ from time import sleep
 import snap7
 from snap7.util import *
 import struct
-
 plc = snap7.client.Client()
-plc.connect("127.0.0.1",0,1)
-
-area = 0x82    # area for Q memory in 
-start = 0      # location we are going to start the read
-length = 1     # length in bytes of the read
-bit = 0        # which bit in the Q memory byte we are reading
-
-byte = plc.read_area(area,0,start,length)
-print ("Q0.0:",get_bool(mbyte,0,bit))
+plc.connect("127.0.0.1",0,1)    
+byte = plc.read_area(0x82,0,0,1)
+print ("Q0.0:",get_bool(mbyte,0,0))
 plc.disconnect()
